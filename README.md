fe7f6a1506be80bfa950f9c84ade5f1b70ed10b4
Scott Corgan <scottcorgan@gmail.com>
2014-03-17 16:51:24 -0700

    Solidify nested suites

--- README.md
+++ README.md
@@ -1,4 +1,56 @@
-tapes
-=====
+# tapes
 
-A more robust tap-producing test harness for node and browsers
+A more robust tap-producing test harness for node and browsers.
+
+Adds the following to [tape]() without changing your normal workflow or adding globals:
+
+* suites/nested tests (betters serparation of test subjects)
+* beforeEach()
+* afterEach()
+
+## Install
+
+```
+npm install tapes --save-dev
+```
+
+## Usage
+
+```js
+var suite = require('tapes');
+
+suite('a set of some tests', function (t) {
+  t.beforeEach(function (t) {
+    // do some set up for each test
+    t.end();
+  });
+  
+  t.afterEach(function (t) {
+    // do some tear down for each test
+    t.end();
+  });
+  
+  t.test('testing something', function (t) {
+    t.ok(true, 'is true');
+    t.end();
+  });
+  
+  t.suite('a nested set of tests', function (t) {
+    t.test('this inherits from the parent suite', function (t) {
+      t.ok(true, 'is true too');
+      t.end();
+    });
+  });
+});
+```
+
+### Running from withing package.json
+
+```js
+{
+  "name": "my-module",
+  "scripts": {
+    "test": "tapes test/**/*.js"
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,83 @@
+var test = require('tape');
+var async = require('async');
+var extend = require('extend');
+var Promise = require('promise');
+
+module.exports = function suite (name, setupRunner, options) {
+  var endNoop = function (t) {t.end();};
+  var afterEach = [];
+  var beforeEach = [];
+  var tests = tests || [];
+  var end = false;
+  
+  // Transfer options to nested suites
+  if (options) {
+    beforeEach = options.beforeEach || [endNoop];
+    afterEach = options.afterEach || [endNoop];
+  }
+  
+  test(name, function (t) {
+    var suiteActions = {};
+    
+    suiteActions.beforeEach = function (fn) {
+      beforeEach.push(fn);
+    };
+    
+    suiteActions.afterEach = function (fn) {
+      afterEach.push(fn);
+    };
+    
+    suiteActions.test = function (name, fn, title) {
+      t.test(name, function (q) {
+        tests.push(new Promise(function (resolve, reject) {
+          runBeforeEach(function () {
+            var end = q.end;
+            
+            q.end = function () {
+              runAfterEach(function () {
+                end.call(q);
+                resolve();
+              });
+            };
+            
+            fn.call(q, q);
+          });
+        }));
+      });
+    };
+    
+    suiteActions.suite = function (name, setupRunner) {
+      suite(name, setupRunner, extend(options, {
+        beforeEach: beforeEach,
+        afterEach: afterEach
+      }));
+    };
+    
+    // Run it all
+    setupRunner(suiteActions);    
+    
+    function runBeforeEach (callback) {
+      async.eachSeries(beforeEach, function (before, done) {
+        before({end: done});
+      }, callback);
+    }
+    
+    function runAfterEach (callback) {
+      async.eachSeries(afterEach, function (after, done) {
+        after({end: done});
+      }, callback);
+    }
+    
+    // All done
+    Promise.all(tests).then(function () {
+      t.end();
+    });
+  });
+
+};
+
+
+// Output json
+// test.createStream({ objectMode: true }).on('data', function (row) {
+//     console.log(JSON.stringify(row))
+// });
