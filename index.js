22207d5514327195cb1bc9c003d459081f1de243
Scott Corgan <scottcorgan@gmail.com>
2014-03-18 15:28:21 -0700

    Make t.plan() work again.

--- index.js
+++ index.js
@@ -19,12 +19,25 @@ var test = function (name, fn, _before, _after) {
     t.test = function (tName, tFn) {
       test(tName, function (q) {
         var qEnd = q.end.bind(q);
+        var qPlan = q.plan.bind(q);
+        var executedAfters = false;
         
         q.end = function () {
           runWrapperFns(after, function () {
+            executedAfters = true;
             qEnd();
           });
         };
+        
+        q.plan = function (numberofAssertions) {
+          qPlan(numberofAssertions);
+        };
+        
+        q.on('end', function (num) {
+          if (!executedAfters) runWrapperFns(after, function () {
+            executedAfters = true;
+          });
+        });
         
         tFn(q);
       }, before.slice(0), after.slice(0));
@@ -37,6 +50,8 @@ var test = function (name, fn, _before, _after) {
 };
 
 function runWrapperFns (fns, callback) {
+  callback = callback || function () {};
+  
   async.eachSeries(fns, function (fn, done) {
     fn({end: done});
   }, callback);
